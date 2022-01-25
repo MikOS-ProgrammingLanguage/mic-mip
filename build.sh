e012c74e6d3bbec8ebe8919d0e801311e35491a6
Glowman554 <glowman434@gmail.com>
2022-01-25 08:27:20 +0100

    some fixes

--- build.sh
+++ build.sh
@@ -25,6 +25,32 @@ function build {
     
 }
 
+
+function install_ {
+    echo -e $(printf "\e[1;32mInstalling $1 ✨\e[0m")
+
+    (
+        cp $1 /bin/$1 2> /dev/null
+    )
+    if [ $? -ne 0 ];
+    then
+        echo -e $(printf "\e[31mFailed install $1 ❌\e[0m")
+        # write error if requested
+        #read var
+        #if [ "$var" = "t" ]
+        #then
+        #    echo -e $(printf "\e[31mError:\e[0m")
+        #    echo "$?"
+        #fi
+        exit 1
+    else
+        echo ✅
+    fi
+    
+}
+
+
+
 if [ "$@" = "mic" ];
 then
     build mic_ mic
@@ -39,4 +65,10 @@ if [ "$@" = "." ];
 then
     build mic_ mic
     build mip_ mip
+fi
+
+if [ "$@" = "install" ];
+then
+    install_ mic
+    install_ mip
 fi
